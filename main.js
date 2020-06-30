7da766a93564eb861a44f112d9624269610eb5c9
Ryan Graham <ryangrahamwpb@gmail.com>
2020-06-30 15:41:38 -0400

    Amend error alert for negative numbers or 0.

--- main.js
+++ main.js
@@ -13,16 +13,16 @@ form.addEventListener("submit", function (e) {
   heightValue = parseInt(heightValue);
   widthValue = parseInt(widthValue);
   if (heightValue >= 100) {
-    alert(" Height cant be greater than 100");
+    alert(" Height can not be greater than 100");
     return false;
-  } else if (heightValue < 0) {
-    alert(" Height cant be a negative number");
+  } else if (heightValue <= 0) {
+    alert(" Height can not be a negative number or 0");
     return false;
   } else if (widthValue >= 100) {
     alert("Width cant be greater than 100");
     return false;
-  } else if (widthValue < 0) {
-    alert(" Width cant be a negative number");
+  } else if (widthValue <= 0) {
+    alert(" Width can not be a negative number or 0");
     return false;
   }
   isValidHex = /^#(?:[0-9a-f]{3}){1,2}$/i;
