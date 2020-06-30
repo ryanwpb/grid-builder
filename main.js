8c10782881be347197d967092b7f018b9a2ea79b
Ryan Graham <ryangrahamwpb@gmail.com>
2020-06-30 13:06:22 -0400

    First commit.

---
+++ main.js
@@ -0,0 +1,46 @@
+const container = document.querySelector("#container");
+
+let form = document.querySelector("#form");
+let heightValue;
+let widthValue;
+let color;
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  heightValue = document.querySelector("#grid-height").value;
+  widthValue = document.querySelector("#grid-width").value;
+  color = document.querySelector("#grid-color").value;
+  heightValue = parseInt(heightValue);
+  widthValue = parseInt(widthValue);
+  if (heightValue >= 100) {
+    alert(" Height cant be greater than 100");
+    return false;
+  } else if (heightValue < 0) {
+    alert(" Height cant be a negative number");
+    return false;
+  } else if (widthValue >= 100) {
+    alert("Width cant be greater than 100");
+    return false;
+  } else if (widthValue < 0) {
+    alert(" Width cant be a negative number");
+    return false;
+  }
+  makeRows(heightValue, widthValue, color);
+});
+
+function makeRows(rows, cols, color) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.innerText = c + 1;
+    cell.style.background = color;
+    container.appendChild(cell).className = "grid-item";
+    cell.addEventListener("click", function () {
+      if (cell.classList.contains("selected")) {
+        cell.classList.remove("selected");
+      } else {
+        cell.classList.add("selected");
+      }
+    });
+  }
+}
